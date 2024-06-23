617a884a7e75123207b3c98bfa29dd7bc12d843f
Adithya S K <adithyaskolavi@gmail.com>
2024-06-23 22:07:53 +0530

    frontend init - Adithya S K

---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import HeroSectionGradientBackground from "@/components/hero";
+
+export default function Home() {
+  return (
+    <HeroSectionGradientBackground />
+  );
+}
